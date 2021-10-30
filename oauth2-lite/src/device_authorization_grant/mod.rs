5ab13d95c24a064ef6983673d45ac7bc26bcecdd
vkill <vkill.net@gmail.com>
2021-10-30 21:30:39 +0800

    Update oauth2-lite

---
+++ oauth2-lite/src/device_authorization_grant/mod.rs
@@ -0,0 +1,8 @@
+//! [RFC 8628](https://datatracker.ietf.org/doc/html/rfc8628)
+
+pub mod device_access_token_endpoint;
+pub mod device_access_token_request;
+pub mod device_access_token_response;
+pub mod device_authorization_endpoint;
+pub mod device_authorization_request;
+pub mod device_authorization_response;
--- oauth2-lite/src/lib.rs
+++ oauth2-lite/src/lib.rs
@@ -1,1 +1,2 @@
-
+#[cfg(feature = "with-device-authorization-grant")]
+pub mod device_authorization_grant;
