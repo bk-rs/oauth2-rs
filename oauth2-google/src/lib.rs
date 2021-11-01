f9fa7c8e89782a512949a3e336e1fa9f55a55dc7
vkill <vkill.net@gmail.com>
2021-11-01 10:53:45 +0800

    Update oauth2_client_device_authorization_grant_demo

--- oauth2-google/src/lib.rs
+++ oauth2-google/src/lib.rs
@@ -1,1 +1,85 @@
+use oauth2_core::{
+    types::{ClientId, ClientSecret, Scope},
+    url::{ParseError as UrlParseError, Url},
+    Provider, ProviderExtDeviceAuthorizationGrant,
+};
+use serde_enum_str::{Deserialize_enum_str, Serialize_enum_str};
 
+pub const TOKEN_URL: &str = "https://oauth2.googleapis.com/token";
+pub const AUTHORIZATION_URL: &str = "https://accounts.google.com/o/oauth2/v2/auth";
+pub const DEVICE_AUTHORIZATION_URL: &str = "https://oauth2.googleapis.com/device/code";
+
+//
+//
+//
+#[derive(Debug, Clone)]
+pub struct GoogleProviderForTvAndDeviceApps {
+    client_id: ClientId,
+    client_secret: ClientSecret,
+    //
+    token_endpoint_url: Url,
+    device_authorization_endpoint_url: Url,
+}
+impl GoogleProviderForTvAndDeviceApps {
+    pub fn new(client_id: ClientId, client_secret: ClientSecret) -> Result<Self, UrlParseError> {
+        Ok(Self {
+            client_id,
+            client_secret,
+            token_endpoint_url: TOKEN_URL.parse()?,
+            device_authorization_endpoint_url: DEVICE_AUTHORIZATION_URL.parse()?,
+        })
+    }
+}
+impl Provider for GoogleProviderForTvAndDeviceApps {
+    type Scope = GoogleScope;
+
+    fn client_id(&self) -> Option<ClientId> {
+        Some(self.client_id.to_owned())
+    }
+
+    fn client_secret(&self) -> Option<ClientSecret> {
+        Some(self.client_secret.to_owned())
+    }
+
+    fn token_endpoint_url(&self) -> Url {
+        self.token_endpoint_url.to_owned()
+    }
+}
+impl ProviderExtDeviceAuthorizationGrant for GoogleProviderForTvAndDeviceApps {
+    fn device_authorization_endpoint_url(&self) -> Url {
+        self.device_authorization_endpoint_url.to_owned()
+    }
+}
+
+//
+//
+//
+#[derive(Deserialize_enum_str, Serialize_enum_str, Debug, Clone, PartialEq)]
+pub enum GoogleScope {
+    //
+    #[serde(rename = "email")]
+    #[serde(alias = "https://www.googleapis.com/auth/userinfo.email")]
+    Email,
+    #[serde(rename = "profile")]
+    #[serde(alias = "https://www.googleapis.com/auth/userinfo.profile")]
+    Profile,
+    //
+    #[serde(rename = "openid")]
+    Openid,
+    //
+    #[serde(rename = "https://www.googleapis.com/auth/drive.file")]
+    DriveFile,
+    //
+    #[serde(rename = "https://www.googleapis.com/auth/youtube")]
+    Youtube,
+    #[serde(rename = "https://www.googleapis.com/auth/youtube.readonly")]
+    YoutubeReadonly,
+    //
+    // TODO
+    //
+    //
+    //
+    #[serde(other)]
+    Other(String),
+}
+impl Scope for GoogleScope {}
---
+++ oauth2_client_device_authorization_grant_demo/src/google.rs
@@ -0,0 +1,40 @@
+/*
+RUST_BACKTRACE=1 RUST_LOG=debug,isahc=off cargo run -p oauth2_client_device_authorization_grant_demo --bin google -- 'YOUR_CLIENT_ID' 'YOUR_CLIENT_SECRET'
+*/
+
+use std::{env, error};
+
+use http_api_isahc_client::IsahcClient;
+use oauth2_client::device_authorization_grant::Flow;
+use oauth2_google::{GoogleProviderForTvAndDeviceApps, GoogleScope};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn error::Error>> {
+    pretty_env_logger::init();
+
+    run().await
+}
+
+async fn run() -> Result<(), Box<dyn error::Error>> {
+    let client_id = env::args().nth(1).unwrap();
+    let client_secret = env::args().nth(2).unwrap();
+    let scopes = vec![GoogleScope::Email, GoogleScope::DriveFile];
+
+    let client = IsahcClient::new()?;
+    let flow = Flow::new(client);
+    let provider = GoogleProviderForTvAndDeviceApps::new(client_id, client_secret)?;
+
+    let access_token_body = flow
+        .start(
+            &provider,
+            scopes,
+            |user_code, verification_uri, _verification_uri_complete| {
+                println!("open [{}] then input [{}]", verification_uri, user_code);
+            },
+        )
+        .await?;
+
+    println!("access_token_body: {:?}", access_token_body);
+
+    Ok(())
+}
