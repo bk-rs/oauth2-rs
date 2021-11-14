5146bf1befca4b29271318ced605d26e75007d41
vkill <vkill.net@gmail.com>
2021-11-14 16:35:59 +0800

    Update

---
+++ examples/client/device_flow/src/microsoft.rs
@@ -0,0 +1,39 @@
+/*
+RUST_BACKTRACE=1 RUST_LOG=debug cargo run -p oauth2_client_device_flow_example --bin device_flow_microsoft -- 'YOUR_CLIENT_ID'
+*/
+
+use std::{env, error};
+
+use http_api_isahc_client::IsahcClient;
+use oauth2_client::device_authorization_grant::Flow;
+use oauth2_microsoft::{MicrosoftProviderForDevices, MicrosoftScope};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn error::Error>> {
+    pretty_env_logger::init();
+
+    let client_id = env::args().nth(1).unwrap();
+
+    run(client_id).await
+}
+
+async fn run(client_id: String) -> Result<(), Box<dyn error::Error>> {
+    let scopes = vec![MicrosoftScope::Openid, MicrosoftScope::Profile, MicrosoftScope::Email];
+
+    let flow = Flow::new(IsahcClient::new()?, IsahcClient::new()?);
+    let provider = MicrosoftProviderForDevices::new("common".to_owned(), client_id)?;
+
+    let access_token_body = flow
+        .execute(
+            &provider,
+            scopes,
+            |user_code, verification_uri, _verification_uri_complete| {
+                println!("open [{}] then input [{}]", verification_uri, user_code);
+            },
+        )
+        .await?;
+
+    println!("access_token_body: {:?}", access_token_body);
+
+    Ok(())
+}
--- providers/oauth2-microsoft/docs/Device_Authorization_Grant.md
+++ providers/oauth2-microsoft/docs/Device_Authorization_Grant.md
@@ -1,3 +1,19 @@
 # Microsoft Device Authorization Grant
 
 Ref [OAuth 2.0 device authorization grant flow](https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-device-code)
+
+## Prerequisites
+
+1. Select / Register the app in the portal
+
+Same as [Authorization_Code_Grant.md](./Authorization_Code_Grant.md)
+
+2. Enable "No keyboard (Device Code Flow)"
+
+In tab "Authentication"
+
+```
+Allow public client flows
+    Enable the following mobile and desktop flows:
+        No keyboard (Device Code Flow) Learn more
+```
